a931b1dbd03602cd9697d465def4fc22f2740c04
sudarsancse <sudarsansarkarcst@gmail.com>
2024-06-06 22:03:30 +0530

    react.js, tailwind css, router dom, react i con and server file install and created

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import Signin from "./Components/Pages/Signin";
+import SignUp from "./Components/Pages/SignUp";
+import Profile from "./Components/Pages/Profile";
+import About from "./Components/Pages/About";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route
+          path="*"
+          element={
+            <h1 className=" text-center text-red-700 text-xl mt-10">
+              {" "}
+              404 page note found{" "}
+            </h1>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
