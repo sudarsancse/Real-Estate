ecc456b0ca5e39e8cf5a122e96546bd87126e39c
sudarsancse <sudarsansarkarcst@gmail.com>
2024-06-07 00:53:36 +0530

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({path: "../.env"});
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:");
+  });
 
 const app = express();
 
 app.listen(5000, () => {
-  console.log("server start on prot number 5000");
+  console.log("Server started on port number 5000");
 });
