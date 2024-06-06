ad284d5a5c0dd230fa49cc99c98673c3177ccedb
sudarsancse <sudarsansarkarcst@gmail.com>
2024-06-07 01:06:41 +0530

    create userModel

---
+++ server/models/UserModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    Password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {timestamps: true}
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
