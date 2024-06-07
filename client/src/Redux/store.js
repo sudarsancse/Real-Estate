384d38e8ce9e85e56043050fdd782557a92bd656
sudarsancse <sudarsansarkarcst@gmail.com>
2024-06-07 18:07:27 +0530

    adding redux persist

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,12 +1,23 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import useReducer from "./User/userSlice";
+import {persistReducer, persistStore} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({user: useReducer});
+
+const parsistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(parsistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: useReducer,
-  },
+  reducer: {persistedReducer},
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {store} from "./Redux/store";
+import {store, persistor} from "./Redux/store";
 import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
